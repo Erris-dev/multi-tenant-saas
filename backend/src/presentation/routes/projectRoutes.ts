98f722da80d593715dd6face10730612abed8ced
Erris Binxhija <143111355+Erris-dev@users.noreply.github.com>
2025-08-24 19:55:25 +0200

    Implemented Rate Limiter for my API

--- backend/src/presentation/routes/projectRoutes.ts
+++ backend/src/presentation/routes/projectRoutes.ts
@@ -5,16 +5,29 @@ import {
   getAllProjectOfUser,
   getSingleProjectController,
 } from "../controllers/projectController";
+import { createLimiter } from "../middleware/rateLimiter";
 import { authCheck } from "../middleware/authMiddleware";
 import { tenantCheck } from "../middleware/tenantMiddleware";
 
 const router = Router();
 
+// Get Routes
 router.get("/", authCheck, tenantCheck, getAllProjectOfUser);
 router.get("/:projectId", authCheck, tenantCheck, getSingleProjectController);
-router.post("/create", authCheck, tenantCheck, createProjectController);
+
+// Create Routes
+router.post(
+  "/create",
+  createLimiter,
+  authCheck,
+  tenantCheck,
+  createProjectController
+);
+
+// Update Routes
 router.patch(
   "/update/:projectId",
+  createLimiter,
   authCheck,
   tenantCheck,
   updateProjectController
--- backend/src/presentation/routes/taskRoutes.ts
+++ backend/src/presentation/routes/taskRoutes.ts
@@ -1,11 +1,24 @@
 import { Router } from "express";
-import { createTaskController, updateTaskController } from "../controllers/taskController";
+import {
+  createTaskController,
+  updateTaskController,
+} from "../controllers/taskController";
 import { authCheck } from "../middleware/authMiddleware";
+import { createLimiter } from "../middleware/rateLimiter";
 import { tenantCheck } from "../middleware/tenantMiddleware";
 
 const router = Router();
 
-router.post('/create/:projectId', authCheck, tenantCheck, createTaskController);
-router.patch('/update/:taskId', authCheck, updateTaskController );
+// Create routes
+router.post(
+  "/create/:projectId",
+  createLimiter,
+  authCheck,
+  tenantCheck,
+  createTaskController
+);
+
+// Update routes
+router.patch("/update/:taskId", createLimiter, authCheck, updateTaskController);
 
 export default router;
